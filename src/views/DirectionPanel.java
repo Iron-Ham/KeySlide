2b1911e40e10d14b9f8036531eea98f73b86818f
Hesham Salman <hesham8@gmail.com>
2015-04-22 10:38:56 -0400

    +Documentation

--- src/views/DirectionPanel.java
+++ src/views/DirectionPanel.java
@@ -10,32 +10,50 @@ import models.*;
 import contracts.InstructionStatus;
 import controllers.InstructionController;
 
+/**
+ * DirectionPanel is the panel that has the direction image.
+ * @author heshamsalman
+ *
+ */
 public class DirectionPanel extends JPanel {
 	InstructionController instrControl = InstructionController.getInstance();
 	private JLabel image;
 	private int internalKey;
 	
-	
+	/**
+	 * Constructor.
+	 */
 	public DirectionPanel() {
 		resolveInstructions();
 		add(image);
 	}
 	
+	/**
+	 * @return internalKey, the KeyEvent code required of it per panel type
+	 */
 	public int getInternalKey() {
 		return internalKey;
 	}
 	
+	/**
+	 * @return returns instructionstatus type
+	 */
 	public InstructionStatus getStatus() {
 		return instrControl.getStatus();
 	}
 	
-	
+	/**
+	 * Switches to the next instruction and updates this panel.
+	 */
 	public void updateDirection() {
 		instrControl.nextInstruction();
 		resolveInstructions();
 		updateUI();
 	}
-	
+
+	/**
+	 * Constructor helper, also helps with updating panel
+	 */
 	private void resolveInstructions() {
 		InstructionStatus status = instrControl.getStatus();
 		if (status == InstructionStatus.LEFT)  {
@@ -78,10 +96,14 @@ public class DirectionPanel extends JPanel {
 			image = new JLabel(icon);
 		}
 	}
-	public void addNotify() {
-		super.addNotify();
-		requestFocus();
-	}
+	
+	/**
+	 * Required if we add a key listener to this class. 
+	 */
+//	public void addNotify() {
+//		super.addNotify();
+//		requestFocus();
+//	}
 	
 	
 }
