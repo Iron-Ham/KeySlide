08774deee53e4ce36c9c2d5fbbbced022518670f
Hesham Salman <hesham8@gmail.com>
2015-04-24 00:39:54 -0400

    NavigationButton: A new abstract class

--- src/views/BackButton.java
+++ src/views/BackButton.java
@@ -1,13 +1,6 @@
 package views;
 
-import java.awt.Font;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
-import utilities.Colors;
 
 /**
  * Back Button allows user to go back to home page when clicked. 
@@ -15,50 +8,18 @@ import utilities.Colors;
  * @author heshamsalman
  *
  */
-public class BackButton extends JButton implements ActionListener, MouseListener {
+public class BackButton extends NavigationButton {
 	private static final long serialVersionUID = 45768139147924018L;
 	Window window;
 
 	public BackButton(Window window) {
-		super("Home");
 		this.window = window;
-		this.setFont(new Font("Arial", Font.PLAIN, 40));
-		this.addActionListener(this);
-		this.addMouseListener(this);
-		this.setContentAreaFilled(false);
-		this.setFocusPainted(false);
-		this.setBorder(BorderFactory.createEmptyBorder());
-		this.setForeground(Colors.unselected());
+		setText("Home");
+		initializeComponents();
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		window.switchToHome();
 	}
-	
-	@Override
-	public void mouseClicked(MouseEvent e) {}
-
-	@Override
-	public void mousePressed(MouseEvent e) {}
-
-	@Override
-	public void mouseReleased(MouseEvent e) {}
-	
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		if (e.getSource() == this) {
-			this.setForeground(Colors.orange());
-		}
-		this.updateUI();
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-		if (e.getSource() == this) {
-			this.setForeground(Colors.unselected());
-		}
-		this.updateUI();
-	}
-	
 }
---
+++ src/views/NavigationButton.java
@@ -0,0 +1,70 @@
+package views;
+
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import utilities.Colors;
+
+/**
+ * The abstract class defining behavior for navigation buttons.
+ * @author heshamsalman
+ *
+ */
+public abstract class NavigationButton extends JButton implements ActionListener, MouseListener {
+	private static final long serialVersionUID = 8971973612959800330L;
+	Window window;
+	
+	@Override
+	public void mouseClicked(MouseEvent e) {}
+
+	@Override
+	public void mousePressed(MouseEvent e) {}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {}
+	
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		if (e.getSource() == this) {
+			this.setForeground(Colors.orange());
+		}
+		this.updateUI();
+	}
+	
+	/**
+	 * Should be ran at initialization of the concrete class.
+	 * Initializes all components of the abstract class.
+	 */
+	protected void initializeComponents() {
+		addListeners();
+		setupLookAndFeel();
+	}
+	
+	private void addListeners() {
+		addActionListener(this);
+		addMouseListener(this);
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		if (e.getSource() == this) {
+			this.setForeground(Colors.unselected());
+		}
+		this.updateUI();
+	}
+	
+	/**
+	 * Sets up UI Feel
+	 */
+	private void setupLookAndFeel() {
+		setFont(new Font("Arial", Font.PLAIN, 40));
+		setContentAreaFilled(false);
+		setFocusPainted(false);
+		setBorder(BorderFactory.createEmptyBorder());
+		setForeground(Colors.unselected());
+	}
+	
+}
